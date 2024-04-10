f7289c924cee8fcfb260e2df0d96d474f21d9014
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-10 16:41:47 +1200

    added bulma and Home.tsx

---
+++ client/components/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="columns">
+      <div className="column">First column</div>
+      <div className="column">Second column</div>
+      <div className="column">Third column</div>
+      <div className="column">Fourth column</div>
+    </div>
+  )
+}
