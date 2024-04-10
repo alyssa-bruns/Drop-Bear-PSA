2ed742a0dca47aeb73629a49ec03a29a953017b1
Alyssa <alynicbru@gmail.com>
2024-04-10 19:02:20 +1200

    create express route to getAllSightings

---
+++ server/routes/sightings.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+
+import * as db from '../db/functions/sightings'
+
+const router = Router()
+
+// GET /api/v1
+router.get('/', async (req, res) => {
+  try {
+    const posts = await db.getAllSightings()
+    res.json(posts)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+})
+
+export default router
