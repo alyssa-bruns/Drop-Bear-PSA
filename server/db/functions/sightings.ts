5b82521b4c492d222671537bb87c687be9e50b4e
Alyssa <alynicbru@gmail.com>
2024-04-10 19:36:40 +1200

    Create getSightingById db function

--- server/db/functions/sightings.ts
+++ server/db/functions/sightings.ts
@@ -6,3 +6,7 @@ const db = connection
 export async function getAllSightings(): Promise<Sighting[]> {
   return db('sightings').select()
 }
+
+export function getSightingById(id: number) {
+  return db('sightings').select().where({ id })
+}
