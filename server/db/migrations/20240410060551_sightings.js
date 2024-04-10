eaf9f9b4396e3f078a53e89f52bafeb85da19ba8
Alyssa <alynicbru@gmail.com>
2024-04-10 18:45:04 +1200

    Add restraints and defaults to sightings table migration

--- server/db/migrations/20240410060551_sightings.js
+++ server/db/migrations/20240410060551_sightings.js
@@ -5,12 +5,12 @@
 export async function up(knex) {
   return knex.schema.createTable('sightings', (table) => {
     table.increments('id')
-    table.string('location')
-    table.date('date')
-    table.time('time')
-    table.string('description')
-    table.string('email')
-    table.boolean('is_approved')
+    table.string('location').notNullable() //location must not be null
+    table.date('date').notNullable() //date must not be null
+    table.time('time').notNullable() //time must not be null
+    table.string('description').notNullable() //encouter description must not be null
+    table.string('email').notNullable //email must not be null
+    table.boolean('is_approved').defaultTo(0)
   })
 }
 /**
