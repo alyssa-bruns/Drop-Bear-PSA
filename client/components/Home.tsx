acf16b9371d6207734655bda288f49a5011e7ccb
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-11 12:42:04 +1200

    added sightingfeed link

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -3,12 +3,7 @@ import Sightingfeed from './Sightingfeed'
 export default function Home() {
   return (
     <>
-      <div className="columns">
-        <div className="column">First column</div>
-        <div className="column">Second column</div>
-        <div className="column">Third column</div>
-        <div className="column">Fourth column</div>
-      </div>
+      <Sightingfeed />
     </>
   )
 }
