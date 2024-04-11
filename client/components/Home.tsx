26c64017f178f01d6b232f0f0ba7c66f44823663
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-11 12:14:22 +1200

    created sighting feed component

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,5 @@
+import Sightingfeed from './Sightingfeed'
+
 export default function Home() {
-  return (
-    <div className="columns">
-      <div className="column">First column</div>
-      <div className="column">Second column</div>
-      <div className="column">Third column</div>
-      <div className="column">Fourth column</div>
-    </div>
-  )
+  return <Sightingfeed />
 }
