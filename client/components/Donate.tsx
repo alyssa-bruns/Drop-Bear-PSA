2f15452c00a5c50cc89b56b668b424d1e23820e4
Alyssa <alynicbru@gmail.com>
2024-04-12 09:40:19 +1200

    Start content for donation page

--- client/components/Donate.tsx
+++ client/components/Donate.tsx
@@ -2,6 +2,24 @@ export function Donate() {
   return (
     <>
       <h2>Donate</h2>
+      <div>
+        <div id="alyssa">
+          <img alt="" src="" />
+          <h3>WildWork Aotearoa</h3>
+          <p>WildWork Aotearoa description</p>
+        </div>
+        <div id="mae">
+          <h3>Charity</h3>
+          <p>charity description</p>
+          <img alt="" src="" />
+        </div>
+
+        <div id="keanu">
+          <img alt="" src="" />
+          <h3>Charity</h3>
+          <p>charity description</p>
+        </div>
+      </div>
     </>
   )
 }
