7dab61232889a1e67179ba90e40daaec6ebafebe
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-11 17:13:07 +1200

    added images and completed functionality of navbar

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,13 +1,58 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 export function Nav() {
+  const [isActive, setisActive] = React.useState(false)
+
   return (
-    <>
-      <nav className="navbar" aria-label="main navigation"></nav>
-      <Link to="/education">Educational Resources</Link>
-      <Link to="/safety">Tourist Safety</Link>
-      <Link to="/news">News</Link>
-      <Link to="/donate">Donate</Link>
-    </>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a href="/" className="navbar-item">
+          <img
+            alt="Drop Bear Logo"
+            // logo here
+          />
+        </a>
+
+        <button
+          onClick={() => {
+            setisActive(!isActive)
+          }}
+          className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+      <div
+        id="navbarBasicExample"
+        className={`navbar-menu ${isActive ? 'is-active' : ''}`}
+      >
+        <div className="navbar-start">
+          <div className="navbar-item">
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+            <Link to="/education" className="navbar-item">
+              Educational
+            </Link>
+            <Link to="/safety" className="navbar-item">
+              Tourist Safety
+            </Link>
+            <Link to="/news" className="navbar-item">
+              News
+            </Link>
+            <Link to="/donate" className="navbar-item">
+              Donate
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
