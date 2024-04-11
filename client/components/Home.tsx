e2c14d9732448838a1aa542c1410c59ffbb2c6f6
Mae BC <maybclaridge@gmail.com>
2024-04-11 15:48:23 +1200

    report button functional

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
 import Sightingfeed from './Sightingfeed'
 
 export default function Home() {
   return (
     <>
       <Sightingfeed />
+      <Link to="/report">
+        <button>Report a Sighting</button>
+      </Link>
     </>
   )
 }
