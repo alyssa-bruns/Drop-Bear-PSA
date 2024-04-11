967eea3b3d28a9c4cccdd0883f8b0e9fcaed3b16
Alyssa <alynicbru@gmail.com>
2024-04-11 12:03:32 +1200

    Finish inputs

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,15 @@
+import SightingReport from './SightingReport'
+
 export default function Home() {
   return (
-    <div className="columns">
-      <div className="column">First column</div>
-      <div className="column">Second column</div>
-      <div className="column">Third column</div>
-      <div className="column">Fourth column</div>
-    </div>
+    <>
+      <div className="columns">
+        <div className="column">First column</div>
+        <div className="column">Second column</div>
+        <div className="column">Third column</div>
+        <div className="column">Fourth column</div>
+      </div>
+      <SightingReport />
+    </>
   )
 }
--- client/components/SightSubmissionForm.tsx
+++ client/components/SightSubmissionForm.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useCallback, useState } from 'react'
 import useAddSighting from '../hooks/use-add-sightings'
 import { useNavigate } from 'react-router-dom'
 
-export function SightSubmissionForm() {
+export default function SightSubmissionForm() {
   const [location, setLocation] = useState('')
   const [date, setDate] = useState('')
   const [time, setTime] = useState('')
@@ -20,15 +20,15 @@ export function SightSubmissionForm() {
     setDate(e.target.value)
   }
 
-  const handleTimeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  const handleTimeChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTime(e.target.value)
   }
 
-  const handleEmailChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value)
   }
 
-  const handleDescriptionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  const handleDescriptionChange = (e: ChangeEvent<HTMLInputElement>) => {
     setDescription(e.target.value)
   }
 
@@ -36,7 +36,11 @@ export function SightSubmissionForm() {
     async (e: React.FormEvent) => {
       e.preventDefault()
       mutation.mutate({
-        description, date, time, location, email,
+        description,
+        date,
+        time,
+        location,
+        email,
       })
       setDescription('')
       setDate('')
@@ -45,8 +49,54 @@ export function SightSubmissionForm() {
       setEmail('')
       navigate('/')
     },
-    [mutation, location, description, email, date, navigate, time]
+    [mutation, location, description, email, date, navigate, time],
   )
-  return(
-
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="location">Location: </label>
+          <input
+            placeholder="place of encounter"
+            onChange={handleLocationChange}
+            value={location}
+            id="location"
+          ></input>
+          <br />
+          <label htmlFor="date">Date: </label>
+          <input
+            placeholder="date of encounter (ex: 2024-04-21)"
+            onChange={handleDateChange}
+            value={date}
+            id="date"
+          ></input>
+          <br />
+          <label htmlFor="time">Time: </label>
+          <input
+            placeholder="time of encounter (ex: 23:00:00)"
+            onChange={handleTimeChange}
+            value={time}
+            id="time"
+          ></input>
+          <br />
+          <label htmlFor="email">Email: </label>
+          <input
+            placeholder="email"
+            onChange={handleEmailChange}
+            value={email}
+            id="email"
+          ></input>
+          <br />
+          <label htmlFor="description">Encounter Description: </label>
+          <input
+            placeholder="Decribe your drop bear encounter with as much detail as you can remember"
+            onChange={handleDescriptionChange}
+            value={description}
+            id="description"
+          ></input>
+          <br />
+        </form>
+      </div>
+    </>
   )
+}
