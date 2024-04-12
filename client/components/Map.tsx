f6961c909c137275e319ea3c2df0c8b88fd36134
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-12 18:25:42 +1200

    added leaflet map

---
+++ client/components/Map.tsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react'
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Popup,
+  Marker,
+  useMapEvents,
+} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const Map = () => {
+  const mapRef = useRef(null)
+  const latitude = -37.2109
+  const longitude = 142.398
+
+  //used for setting a marker location
+  // const position = [51.505, -0.09]
+
+  return (
+    // Make sure you set the height and width of the map container otherwise the map won't show
+    <MapContainer
+      center={[latitude, longitude]}
+      zoom={13}
+      ref={mapRef}
+      style={{ height: '30vh', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker> */}
+      <LocationFinder />
+    </MapContainer>
+  )
+}
+
+export default Map
