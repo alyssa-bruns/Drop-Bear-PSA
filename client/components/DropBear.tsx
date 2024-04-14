09fba5a6dcaa5f6e83a259b3d5d6862f3b32736e
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-14 12:40:52 +1200

    Create illustion of mulitple boxes

--- client/components/DropBear.tsx
+++ client/components/DropBear.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/no-unknown-property */
 import * as THREE from 'three'
 import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 
 function Box() {
   const ref = useRef()
+  const { viewport } = useThree()
 
   useFrame(() => {
     ref.current.position.y += 0.1
-    if (ref.current.position.y > 3) {
-      ref.current.position.y = -3
+    if (ref.current.position.y > viewport.height / 1.5) {
+      ref.current.position.y = -viewport.height / 1.5
     }
   })
 
@@ -23,6 +24,8 @@ function Box() {
 export function DropBear() {
   return (
     <Canvas>
+      <Box />
+      <Box />
       <Box />
     </Canvas>
   )
