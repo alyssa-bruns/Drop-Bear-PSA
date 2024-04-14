eda5173b54e708b9625ea8959524e5712aa3b3ff
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-14 15:11:32 +1200

    get model displaying with lighting

--- client/components/DropBear.tsx
+++ client/components/DropBear.tsx
@@ -2,43 +2,46 @@
 import * as THREE from 'three'
 import { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, Environment } from '@react-three/drei'
 
-function Box({ z }) {
-  const ref = useRef()
-  const { viewport, camera } = useThree()
-  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
+// function Box({ z }) {
+//   const ref = useRef()
+//   const { viewport, camera } = useThree()
+//   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
 
-  const [data] = useState({
-    x: THREE.MathUtils.randFloatSpread(2),
-    y: THREE.MathUtils.randFloatSpread(height),
-  })
+//   const [data] = useState({
+//     x: THREE.MathUtils.randFloatSpread(2),
+//     y: THREE.MathUtils.randFloatSpread(height),
+//   })
 
-  useFrame(() => {
-    ref.current.position.set(data.x * width, (data.y += 0.3), z)
-    if (data.y > height / 1.5) {
-      data.y = -height / 1.5
-    }
-  })
+//   useFrame(() => {
+//     ref.current.position.set(data.x * width, (data.y += 0.3), z)
+//     if (data.y > height / 1.5) {
+//       data.y = -height / 1.5
+//     }
+//   })
 
-  return (
-    <mesh ref={ref}>
-      <boxGeometry />
-      <meshBasicMaterial color="purple" />
-    </mesh>
-  )
-}
+//   return (
+//     <mesh ref={ref}>
+//       <boxGeometry />
+//       <meshBasicMaterial color="purple" />
+//     </mesh>
+//   )
+// }
 
-function Model() {
-  const { scene } = useGLTF('../images/three/koala_from_polu_by_google.gltf')
-  return <primitive object={scene} />
+function Model(props) {
+  const { scene } = useGLTF('../images/koala_from_poly_by_google/scene.gltf')
+  return <primitive object={scene} {...props} />
 }
 
 export function DropBear({ count = 100 }) {
   return (
     <Canvas>
+      <ambientLight intensity={0.2} />
+      <spotLight position={[10, 10, 10]} intensity={2} />
       <Suspense fallback={null}>
-        <Model />
+        <Model scale={0.5} />
+        <Environment preset="sunset" />
       </Suspense>
       {/* {Array.from({ length: count }, (_, i) => (
         <Box key={i} z={-i} />
