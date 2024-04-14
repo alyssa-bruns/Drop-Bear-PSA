acb2793b05302530152a1671308d68a606bab39a
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-14 16:20:30 +1200

    Edit effects

--- client/components/DropBear.tsx
+++ client/components/DropBear.tsx
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useGLTF, Environment } from '@react-three/drei'
-import { zipSync } from 'three/examples/jsm/libs/fflate.module.js'
+import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
 
 function Model({ z }) {
   const ref = useRef()
@@ -102,14 +102,23 @@ useGLTF.preload('../images/koala_from_poly_by_google/scene.gltf')
 
 export function DropBear({ count = 100 }) {
   return (
-    <Canvas>
-      <ambientLight intensity={0.2} />
-      <spotLight position={[10, 10, 10]} intensity={2} />
+    <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 110 }}>
+      <color attach="background" args={['#30336b']} />
+
+      <spotLight position={[10, 10, 10]} intensity={1} />
       <Suspense fallback={null}>
+        <Environment preset="sunset" />
         {Array.from({ length: count }, (_, i) => (
           <Model key={i} z={-i} />
         ))}
-        <Environment preset="sunset" />
+        <EffectComposer>
+          <DepthOfField
+            target={[0, 0, 30]}
+            focalLength={0.5}
+            bokehScale={4}
+            height={700}
+          />
+        </EffectComposer>
       </Suspense>
     </Canvas>
   )
