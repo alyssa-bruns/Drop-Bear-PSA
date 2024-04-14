ff3b495112914756c5dfdd0c3e3cc4fc44d1ea88
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-14 15:38:38 +1200

    Replace boxes with koalas

--- client/components/DropBear.tsx
+++ client/components/DropBear.tsx
@@ -3,9 +3,13 @@ import * as THREE from 'three'
 import { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useGLTF, Environment } from '@react-three/drei'
+import { zipSync } from 'three/examples/jsm/libs/fflate.module.js'
 
-function Box({ z }) {
+function Model({ z }) {
   const ref = useRef()
+  const { nodes, materials } = useGLTF(
+    '../images/koala_from_poly_by_google/scene.gltf',
+  )
   const { viewport, camera } = useThree()
   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
 
@@ -22,20 +26,7 @@ function Box({ z }) {
   })
 
   return (
-    <mesh ref={ref}>
-      <boxGeometry />
-      <meshBasicMaterial color="purple" />
-    </mesh>
-  )
-}
-
-// GLTF file transformed into React Component using https://gltf.pmnd.rs/
-export function Model({ ...props }) {
-  const { nodes, materials } = useGLTF(
-    '../images/koala_from_poly_by_google/scene.gltf',
-  )
-  return (
-    <group {...props} dispose={null}>
+    <group ref={ref} dispose={null}>
       <mesh
         castShadow
         receiveShadow
@@ -64,6 +55,41 @@ export function Model({ ...props }) {
   )
 }
 
+// GLTF file transformed into React Component using https://gltf.pmnd.rs/
+// export function Model({ ...props }) {
+//   const { nodes, materials } = useGLTF(
+//     '../images/koala_from_poly_by_google/scene.gltf',
+//   )
+//   return (
+//     <group {...props} dispose={null}>
+//       <mesh
+//         castShadow
+//         receiveShadow
+//         geometry={nodes.Object_4.geometry}
+//         material={materials['455A64']}
+//       />
+//       <mesh
+//         castShadow
+//         receiveShadow
+//         geometry={nodes.Object_5.geometry}
+//         material={materials.FFFFFF}
+//       />
+//       <mesh
+//         castShadow
+//         receiveShadow
+//         geometry={nodes.Object_6.geometry}
+//         material={materials['1A1A1A']}
+//       />
+//       <mesh
+//         castShadow
+//         receiveShadow
+//         geometry={nodes.Object_7.geometry}
+//         material={materials.F06292}
+//       />
+//     </group>
+//   )
+// }
+
 useGLTF.preload('../images/koala_from_poly_by_google/scene.gltf')
 
 export function DropBear({ count = 100 }) {
@@ -72,13 +98,11 @@ export function DropBear({ count = 100 }) {
       <ambientLight intensity={0.2} />
       <spotLight position={[10, 10, 10]} intensity={2} />
       <Suspense fallback={null}>
-        <Model scale={0.5} />
-        <Model scale={0.5} position={[1, 0, -1]} />
+        {Array.from({ length: count }, (_, i) => (
+          <Model key={i} z={-i} />
+        ))}
         <Environment preset="sunset" />
       </Suspense>
-      {Array.from({ length: count }, (_, i) => (
-        <Box key={i} z={-i} />
-      ))}
     </Canvas>
   )
 }
