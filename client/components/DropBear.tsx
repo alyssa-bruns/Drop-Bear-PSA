7d0b35b98b97c6badcc39c64ff9b9a6a44d1de04
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-14 12:49:17 +1200

    Make viewport responsive

--- client/components/DropBear.tsx
+++ client/components/DropBear.tsx
@@ -8,12 +8,12 @@ function Box() {
   const { viewport } = useThree()
 
   const [data] = useState({
-    x: THREE.MathUtils.randFloatSpread(viewport.width),
+    x: THREE.MathUtils.randFloatSpread(2),
     y: 0,
   })
 
   useFrame(() => {
-    ref.current.position.set(data.x, (data.y += 0.1), 0)
+    ref.current.position.set(data.x * viewport.width, (data.y += 0.1), 0)
     if (data.y > viewport.height / 1.5) {
       data.y = -viewport.height / 1.5
     }
