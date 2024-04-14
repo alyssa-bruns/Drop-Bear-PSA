c51ab67a88e7f18902f36f0aff6e6485e851c00a
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-14 15:45:41 +1200

    Set individual rotations for each koala

--- client/components/DropBear.tsx
+++ client/components/DropBear.tsx
@@ -16,10 +16,13 @@ function Model({ z }) {
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
     y: THREE.MathUtils.randFloatSpread(height),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
   })
 
   useFrame((state) => {
-    ref.current.rotation.y += 0.01
+    ref.current.rotation.set(data.rX, data.rY, data.rZ)
     ref.current.position.set(data.x * width, (data.y += 0.1), z)
     if (data.y > height / 1.5) {
       data.y = -height / 1.5
