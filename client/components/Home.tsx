4d96d07967f52ab60170b5a64f6cc72a019b4b75
Mae BC <maybclaridge@gmail.com>
2024-04-15 16:37:48 +1200

    quick save

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -2,19 +2,30 @@ import { Link } from 'react-router-dom'
 import Sightingfeed from './Sightingfeed'
 import Map from './Map'
 import SightSubmissionForm from './SightSubmissionForm'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <>
-      <div className="columns">
-        <div className="column is-three-fifths is-offset-one-fifth">
-          <Map />
-          <Sightingfeed />
-          <Link to="/report">
-            <button>Report a Sighting</button>
-          </Link>
-        </div>
-      </div>
+      <AnimatePresence>
+        <motion.div
+          initial={{ y: 25, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 0.75,
+          }}
+        >
+          <div className="columns">
+            <div className="column is-three-fifths is-offset-one-fifth">
+              <Map />
+              <Sightingfeed />
+              <Link to="/report">
+                <button>Report a Sighting</button>
+              </Link>
+            </div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   )
 }
