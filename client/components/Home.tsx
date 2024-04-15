c23c828bcd40d0f425c4f09291451ee07bd31b30
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-15 18:29:00 +1200

    made sighting report form hide and show onclick

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,44 @@
 import { Link } from 'react-router-dom'
 import Sightingfeed from './Sightingfeed'
+import SightingReport from './SightingReport'
 import Map from './Map'
 import SightSubmissionForm from './SightSubmissionForm'
 import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
 
 export default function Home() {
+  const [isReporting, setIsReporting] = useState(false)
+
+  if (isReporting) {
+    return (
+      <>
+        <AnimatePresence>
+          <motion.div
+            initial={{ y: 25, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 0.75,
+            }}
+          >
+            <div className="columns">
+              <div className="column is-three-fifths is-offset-one-fifth">
+                <Map />
+                <button
+                  onClick={() => {
+                    setIsReporting(false)
+                  }}
+                >
+                  cancel
+                </button>
+                <SightingReport />
+                <Sightingfeed />
+              </div>
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      </>
+    )
+  }
   return (
     <>
       <AnimatePresence>
@@ -18,10 +52,17 @@ export default function Home() {
           <div className="columns">
             <div className="column is-three-fifths is-offset-one-fifth">
               <Map />
+              <div className="buttons is-centered m-3">
+                <button
+                  className="button is-large"
+                  onClick={() => {
+                    setIsReporting(true)
+                  }}
+                >
+                  Add your own drop bear sighting!
+                </button>
+              </div>
               <Sightingfeed />
-              <Link to="/home/report">
-                <button>Report a Sighting</button>
-              </Link>
             </div>
           </div>
         </motion.div>
