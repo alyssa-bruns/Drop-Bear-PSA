56d96111ebef63bbb18a9a0ecd45f269ef7170c2
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-16 13:37:23 +1200

    Play with landing page button

--- client/components/LandingPage.tsx
+++ client/components/LandingPage.tsx
@@ -8,7 +8,10 @@ export function LandingPage() {
       <div className="landing-page">
         <div className="centered">
           <img src="../images/logo1.png" alt="logo" />
-          <Link className="button" to="/home">
+          <Link
+            className="button  is-inverted is-large is-success is-responsive"
+            to="/home"
+          >
             Enter
           </Link>
         </div>
