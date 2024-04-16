e3cf59796f3aa9fb45186a3c9c905d6669cb9ec1
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-04-16 13:01:59 +1200

    Add formatting and animation

--- client/components/SightingsByLocation.tsx
+++ client/components/SightingsByLocation.tsx
@@ -1,29 +1,51 @@
 import { useParams } from 'react-router-dom'
 import { useGetSightingsByLocation } from '../hooks/use-get-sightings'
+import Map from './Map'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function SightingsByLocation() {
   const { location } = useParams()
   const { data } = useGetSightingsByLocation(String(location))
 
   return (
-    <ul>
-      {data &&
-        data.map((sighting) =>
-          sighting.is_approved ? (
-            <div className="card" key={sighting.id}>
-              <header className="card-header">
-                <p className="card-header-title">{sighting.display_name}</p>
-                <br />
-              </header>
-              <p className="card-header-title is-size-7">
-                Sighting recorded at {sighting.time} on {sighting.date}
-              </p>
-              <div className="card-content">
-                <div className="content">{sighting.description}</div>
-              </div>
+    <>
+      <AnimatePresence>
+        <motion.div
+          initial={{ y: 25, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 0.75,
+          }}
+        >
+          <div className="columns">
+            <div className="column is-three-fifths is-offset-one-fifth">
+              <Map />
+              <ul>
+                {data &&
+                  data.map((sighting) =>
+                    sighting.is_approved ? (
+                      <div className="card" key={sighting.id}>
+                        <header className="card-header">
+                          <p className="card-header-title">
+                            {sighting.display_name}
+                          </p>
+                          <br />
+                        </header>
+                        <p className="card-header-title is-size-7">
+                          Sighting recorded at {sighting.time} on{' '}
+                          {sighting.date}
+                        </p>
+                        <div className="card-content">
+                          <div className="content">{sighting.description}</div>
+                        </div>
+                      </div>
+                    ) : null,
+                  )}
+              </ul>
             </div>
-          ) : null,
-        )}
-    </ul>
+          </div>
+        </motion.div>
+      </AnimatePresence>
+    </>
   )
 }
